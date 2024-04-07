a6e2b80b69236c8efb0c1cbba1b5e88f6f7136a8
Rohit Nair <rohitn@dnat.simula.no>
2024-04-07 15:10:41 +0200

    File Structure PARSeq

--- GroupNet/model/PARSeq/decoder.py
+++ GroupNet/model/PARSeq/decoder.py
@@ -1,36 +1,50 @@
-# Scene Text Recognition Model Hub
-# Copyright 2022 Darwin Bautista
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-import math
-from typing import Optional
-
+from torch import Tensor
+from typing import Optional, Sequence
+from model.head import get_activation
+import lightning.pytorch as pl
+import torch.nn as nn
 import torch
-from torch import nn as nn, Tensor
-from torch.nn import functional as F
-from torch.nn.modules import transformer
+import math
 
+class DecoderLayer(pl.LightningModule):
+    """
+    A Transformer decoder layer supporting two-stream attention (XLNet)
+    This implements a pre-LN decoder, as opposed to the post-LN default in PyTorch.
+    """
 
-class DecoderLayer(nn.Module):
-    """A Transformer decoder layer supporting two-stream attention (XLNet)
-       This implements a pre-LN decoder, as opposed to the post-LN default in PyTorch."""
+    def __init__(self, d_model, nhead, dim_feedforward=2048, dropout=0.1, activation='GELU',
+                 layer_norm_eps=1e-5, num_h_c:int = 2, num_d_c:int = 2):
+        """
+        constructor for DecoderLayer
+        Args:
+        - d_model (int): dimension of the decoder feature vectors
 
-    def __init__(self, d_model, nhead, dim_feedforward=2048, dropout=0.1, activation='gelu',
-                 layer_norm_eps=1e-5):
+        """
         super().__init__()
-        self.self_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout, batch_first=True)
-        self.cross_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout, batch_first=True)
+        self.num_h_c = num_h_c
+        self.num_d_c = num_d_c
+        self.self_attn_h_c = [nn.MultiheadAttention(d_model//(num_h_c * 3), nhead / self.num_h_c, 
+                                                    dropout= dropout, batch_first= True) for _ in range(num_h_c)]
+        for _ in range(self.num_h_c):
+            # 0th index will correspond to h_c_n and ith index will correspond to h_c_(n-i)
+            self.self_attn_h_c.append(
+                                        nn.MultiheadAttention(d_model//(num_h_c * 3), nhead / self.num_h_c, 
+                                                                dropout= dropout, batch_first= True)
+                                    )
+        
+        self.self_attn_f_c = nn.MultiheadAttention(d_model // 3, nhead, dropout=dropout, batch_first=True)
+        self.self_attn_d_c = [nn.MultiheadAttention(d_model // (num_d_c * 3), nhead / num_d_c,
+                                                    dropout=dropout, batch_first=True) for _ in range(num_d_c)]
+        self.cross_attn = nn.MultiheadAttention(d_model, nhead * 3, dropout=dropout, batch_first=True)
+
+        # for merging representations
+        self.merge1 = nn.Linear(d_model, dim_feedforward)
+        self.activation_merge = get_activation(activation)()
+        self.dropout_merge1 = nn.Dropout(dropout)
+        self.merge2 = nn.Linear(dim_feedforward, d_model)
+        self.dropout_merge2 = nn.Dropout(dropout)
+        self.norm_merge = nn.LayerNorm(d_model, eps= layer_norm_eps)
+
         # Implementation of Feedforward model
         self.linear1 = nn.Linear(d_model, dim_feedforward)
         self.dropout = nn.Dropout(dropout)
@@ -39,71 +53,96 @@ class DecoderLayer(nn.Module):
         self.norm1 = nn.LayerNorm(d_model, eps=layer_norm_eps)
         self.norm2 = nn.LayerNorm(d_model, eps=layer_norm_eps)
         self.norm_q = nn.LayerNorm(d_model, eps=layer_norm_eps)
-        self.norm_c = nn.LayerNorm(d_model, eps=layer_norm_eps)
+        self.norm_contxt_h_c = [nn.LayerNorm(d_model, eps=layer_norm_eps) for _ in range(num_h_c)]
+        self.norm_contxt_f_c = nn.LayerNorm(d_model, eps=layer_norm_eps)
+        self.norm_contxt_d_c = [nn.LayerNorm(d_model, eps=layer_norm_eps) for _ in range(num_d_c)]
+
         self.dropout1 = nn.Dropout(dropout)
         self.dropout2 = nn.Dropout(dropout)
         self.dropout3 = nn.Dropout(dropout)
 
-        self.activation = transformer._get_activation_fn(activation)
+        self.activation = get_activation(activation)()
 
-    def __setstate__(self, state):
-        if 'activation' not in state:
-            state['activation'] = F.gelu
-        super().__setstate__(state)
-
-    def forward_stream(self, tgt: Tensor, tgt_norm: Tensor, tgt_kv: Tensor, memory: Tensor, tgt_mask: Optional[Tensor],
-                       tgt_key_padding_mask: Optional[Tensor]):
-        """Forward pass for a single stream (i.e. content or query)
+    def forward_stream(self, tgt: Tensor, tgt_norm: Tensor, tgt_h_c_kv: tuple, tgt_f_c_kv:Tensor, tgt_d_c_kv:Tensor,
+                       memory: Tensor, tgt_mask: Optional[Tensor], tgt_key_padding_mask: Optional[Tensor]):
+        """
+        Forward pass for a single stream (i.e. content or query)
         tgt_norm is just a LayerNorm'd tgt. Added as a separate parameter for efficiency.
         Both tgt_kv and memory are expected to be LayerNorm'd too.
         memory is LayerNorm'd by ViT.
         """
-        tgt2, sa_weights = self.self_attn(tgt_norm, tgt_kv, tgt_kv, attn_mask=tgt_mask,
-                                          key_padding_mask=tgt_key_padding_mask)
-        tgt = tgt + self.dropout1(tgt2)
-
+        # self-attn
+        tgt2_h_c, sa_weights_h_c = [], []
+        for self_attn, tgt_h_c in zip(self.self_attn_h_c, tgt_h_c_kv):
+            tgt_, sa_ = self_attn(tgt_norm, tgt_h_c, attn_mask=tgt_mask, key_padding_mask=tgt_key_padding_mask)
+            tgt2.append(tgt_)
+            sa_weights_h_c.append(sa_)
+        tgt2_f_c, sa_weights_f_c = self.self_attn_f_c(tgt_norm, tgt_f_c_kv, attn_mask=tgt_mask, key_padding_mask=tgt_key_padding_mask)
+        tgt2_d_c, sa_weights_d_c = []
+        for self_attn, tgt_d_c in zip(self.self_attn_d_c, tgt_d_c_kv):
+            tgt_, sa_ = self.self_attn_d_c(tgt_norm, tgt_d_c, attn_mask=tgt_mask, key_padding_mask=tgt_key_padding_mask)
+            tgt2_d_c.append(tgt_)
+            sa_weights_d_c.append(sa_)
+        
+        # Merge
+        tgt2 = torch.cat([*tgt2_h_c, tgt2_f_c, *tgt2_d_c], dim= -1)
+        sa_weights = (tuple(sa_weights_h_c), sa_weights_f_c, sa_weights_d_c)
+        tgt2 = self.merge2(self.dropout_merge1(self.activation_merge(self.merge1(self.norm_merge(tgt2)))))
+        tgt = tgt + self.dropout_merge2(tgt2)
+        
+        # cross attn
         tgt2, ca_weights = self.cross_attn(self.norm1(tgt), memory, memory)
         tgt = tgt + self.dropout2(tgt2)
 
+        # MLP
         tgt2 = self.linear2(self.dropout(self.activation(self.linear1(self.norm2(tgt)))))
         tgt = tgt + self.dropout3(tgt2)
         return tgt, sa_weights, ca_weights
 
-    def forward(self, query, content, memory, query_mask: Optional[Tensor] = None, content_mask: Optional[Tensor] = None,
-                content_key_padding_mask: Optional[Tensor] = None, update_content: bool = True):
+    def forward(self, query:Tensor, context_h_c:Sequence[Tensor], context_f_c:Tensor, context_d_c:Sequence[Tensor], 
+                memory:Tensor, query_mask: Optional[Tensor] = None, context_key_padding_mask: Optional[Tensor] = None):
         query_norm = self.norm_q(query)
-        content_norm = self.norm_c(content)
-        query = self.forward_stream(query, query_norm, content_norm, memory, query_mask, content_key_padding_mask)[0]
-        if update_content:
-            content = self.forward_stream(content, content_norm, content_norm, memory, content_mask,
-                                          content_key_padding_mask)[0]
-        return query, content
+        context_h_c_norm = (self.norm_context_h_c[i](context_h_c_i) for i, context_h_c_i in enumerate(context_h_c))
+        context_f_c_norm = self.norm_contxt_f_c(context_f_c)
+        context_d_c_norm = (self.norm_contxt_d_c[i](context_d_c_i) for i, context_d_c_i in enumerate(context_d_c))
+        query = self.forward_stream(query, query_norm, context_h_c_norm, context_f_c_norm, context_d_c_norm,
+                                    memory, query_mask, context_key_padding_mask)[0]
+        return query
 
-
-class Decoder(nn.Module):
+class Decoder(pl.LightningModule):
     __constants__ = ['norm']
 
-    def __init__(self, decoder_layer, num_layers, norm):
+    def __init__(self, d_model:int , nhead:int, dim_feedforward= 2048, dropout= 0.1, activation='GELU', 
+                 layer_norm_eps=1e-5, num_h_c:int = 2, num_d_c:int = 2, num_layers:int = 1):
         super().__init__()
-        self.layers = transformer._get_clones(decoder_layer, num_layers)
+        self.layers = [DecoderLayer(d_model, nhead, dim_feedforward=dim_feedforward,
+                                    dropout= dropout, activation= activation,
+                                    layer_norm_eps=layer_norm_eps, 
+                                    num_h_c = num_h_c, num_d_c= num_d_c) for _ in range(num_layers)]
         self.num_layers = num_layers
-        self.norm = norm
+        self.norm = nn.LayerNorm(d_model)
 
-    def forward(self, query, content, memory, query_mask: Optional[Tensor] = None, content_mask: Optional[Tensor] = None,
-                content_key_padding_mask: Optional[Tensor] = None):
-        for i, mod in enumerate(self.layers):
-            last = i == len(self.layers) - 1
-            query, content = mod(query, content, memory, query_mask, content_mask, content_key_padding_mask,
-                                 update_content=not last)
+    def forward(self, query:Tensor, context_h_c:Sequence[Tensor], context_f_c:Tensor, context_d_c:Sequence[Tensor],
+                memory, query_mask: Optional[Tensor] = None, context_key_padding_mask: Optional[Tensor] = None):
+        for layer in self.layers:
+            query = layer(query, context_h_c, context_f_c, context_d_c, memory, query_mask, context_key_padding_mask)
         query = self.norm(query)
         return query
 
-class TokenEmbedding(nn.Module):
+class TokenEmbedding(pl.LightningModule):
 
-    def __init__(self, charset_size: int, embed_dim: int):
+    def __init__(self, h_c_charset_size: int, f_c_charset_size:int, d_c_charset_size:int, 
+                 embed_dim: int, num_h_c:int = 2, num_d_c:int = 2):
         super().__init__()
-        self.embedding = nn.Embedding(charset_size, embed_dim)
+        self.h_c_embedding = [nn.Embedding(num_embeddings=h_c_charset_size, embedding_dim=embed_dim)
+                                for _ in range(num_h_c)]
+        self.f_c_embedding = nn.Embedding(num_embeddings=f_c_charset_size, embedding_dim=embed_dim)
+        self.d_c_embedding = [nn.Embedding(num_embeddings=d_c_charset_size, embedding_dim=embed_dim)
+                                for _ in range(num_d_c)]
         self.embed_dim = embed_dim
 
-    def forward(self, tokens: torch.Tensor):
-        return math.sqrt(self.embed_dim) * self.embedding(tokens)
+    def forward(self, h_c_tokens: Sequence[Tensor], f_c_tokens:Tensor, d_c_tokens:Sequence[Tensor]):
+        h_c_emb = [math.sqrt(self.embed_dim) * self.h_c_embedding[i](h_c_t) for i,h_c_t in enumerate(h_c_tokens)]
+        f_c_emb = math.sqrt(self.embed_dim) * self.f_c_embedding(f_c_tokens)
+        d_c_emb = [math.sqrt(self.embed_dim) * self.d_c_embedding[i](d_c_t) for i, d_c_t in enumerate(d_c_tokens)]
+        return h_c_emb, f_c_emb, d_c_emb
--- GroupNet/model/PARSeq/net.py
+++ GroupNet/model/PARSeq/net.py
@@ -1,153 +1,13 @@
 from model.base import HindiBaseSystem, MalayalamBaseSystem
 from model.FocalSTR.encoder import FocalNetEncoder
 from torch import Tensor
-from model.head import get_activation
-from typing import Optional, Sequence, Union
+from typing import Optional, Sequence
 from itertools import permutations
-import lightning.pytorch as pl
+from .decoder import Decoder, TokenEmbedding
 import torch.nn as nn
 import torch
 import numpy as np
 import math
-
-class DecoderLayer(pl.LightningModule):
-    """A Transformer decoder layer supporting two-stream attention (XLNet)
-       This implements a pre-LN decoder, as opposed to the post-LN default in PyTorch."""
-
-    def __init__(self, d_model, nhead, dim_feedforward=2048, dropout=0.1, activation='GELU',
-                 layer_norm_eps=1e-5, num_h_c:int = 2, num_d_c:int = 2):
-        """
-        constructor for DecoderLayer
-        Args:
-        - d_model (int): dimension of the decoder feature vectors
-
-        """
-        super().__init__()
-        self.num_h_c = num_h_c
-        self.num_d_c = num_d_c
-        self.self_attn_h_c = [nn.MultiheadAttention(d_model//(num_h_c * 3), nhead / self.num_h_c, 
-                                                    dropout= dropout, batch_first= True) for _ in range(num_h_c)]
-        for _ in range(self.num_h_c):
-            # 0th index will correspond to h_c_n and ith index will correspond to h_c_(n-i)
-            self.self_attn_h_c.append(
-                                        nn.MultiheadAttention(d_model//(num_h_c * 3), nhead / self.num_h_c, 
-                                                                dropout= dropout, batch_first= True)
-                                    )
-        
-        self.self_attn_f_c = nn.MultiheadAttention(d_model // 3, nhead, dropout=dropout, batch_first=True)
-        self.self_attn_d_c = [nn.MultiheadAttention(d_model // (num_d_c * 3), nhead / num_d_c,
-                                                    dropout=dropout, batch_first=True) for _ in range(num_d_c)]
-        self.cross_attn = nn.MultiheadAttention(d_model, nhead * 3, dropout=dropout, batch_first=True)
-
-        # for merging representations
-        self.merge1 = nn.Linear(d_model, dim_feedforward)
-        self.activation_merge = get_activation(activation)()
-        self.dropout_merge1 = nn.Dropout(dropout)
-        self.merge2 = nn.Linear(dim_feedforward, d_model)
-        self.dropout_merge2 = nn.Dropout(dropout)
-        self.norm_merge = nn.LayerNorm(d_model, eps= layer_norm_eps)
-
-        # Implementation of Feedforward model
-        self.linear1 = nn.Linear(d_model, dim_feedforward)
-        self.dropout = nn.Dropout(dropout)
-        self.linear2 = nn.Linear(dim_feedforward, d_model)
-
-        self.norm1 = nn.LayerNorm(d_model, eps=layer_norm_eps)
-        self.norm2 = nn.LayerNorm(d_model, eps=layer_norm_eps)
-        self.norm_q = nn.LayerNorm(d_model, eps=layer_norm_eps)
-        self.norm_contxt_h_c = [nn.LayerNorm(d_model, eps=layer_norm_eps) for _ in range(num_h_c)]
-        self.norm_contxt_f_c = nn.LayerNorm(d_model, eps=layer_norm_eps)
-        self.norm_contxt_d_c = [nn.LayerNorm(d_model, eps=layer_norm_eps) for _ in range(num_d_c)]
-
-        self.dropout1 = nn.Dropout(dropout)
-        self.dropout2 = nn.Dropout(dropout)
-        self.dropout3 = nn.Dropout(dropout)
-
-        self.activation = get_activation(activation)()
-
-    def forward_stream(self, tgt: Tensor, tgt_norm: Tensor, tgt_h_c_kv: tuple, tgt_f_c_kv:Tensor, tgt_d_c_kv:Tensor,
-                       memory: Tensor, tgt_mask: Optional[Tensor], tgt_key_padding_mask: Optional[Tensor]):
-        """
-        Forward pass for a single stream (i.e. content or query)
-        tgt_norm is just a LayerNorm'd tgt. Added as a separate parameter for efficiency.
-        Both tgt_kv and memory are expected to be LayerNorm'd too.
-        memory is LayerNorm'd by ViT.
-        """
-        # self-attn
-        tgt2_h_c, sa_weights_h_c = [], []
-        for self_attn, tgt_h_c in zip(self.self_attn_h_c, tgt_h_c_kv):
-            tgt_, sa_ = self_attn(tgt_norm, tgt_h_c, attn_mask=tgt_mask, key_padding_mask=tgt_key_padding_mask)
-            tgt2.append(tgt_)
-            sa_weights_h_c.append(sa_)
-        tgt2_f_c, sa_weights_f_c = self.self_attn_f_c(tgt_norm, tgt_f_c_kv, attn_mask=tgt_mask, key_padding_mask=tgt_key_padding_mask)
-        tgt2_d_c, sa_weights_d_c = []
-        for self_attn, tgt_d_c in zip(self.self_attn_d_c, tgt_d_c_kv):
-            tgt_, sa_ = self.self_attn_d_c(tgt_norm, tgt_d_c, attn_mask=tgt_mask, key_padding_mask=tgt_key_padding_mask)
-            tgt2_d_c.append(tgt_)
-            sa_weights_d_c.append(sa_)
-        
-        # Merge
-        tgt2 = torch.cat([*tgt2_h_c, tgt2_f_c, *tgt2_d_c], dim= -1)
-        sa_weights = (tuple(sa_weights_h_c), sa_weights_f_c, sa_weights_d_c)
-        tgt2 = self.merge2(self.dropout_merge1(self.activation_merge(self.merge1(self.norm_merge(tgt2)))))
-        tgt = tgt + self.dropout_merge2(tgt2)
-        
-        # cross attn
-        tgt2, ca_weights = self.cross_attn(self.norm1(tgt), memory, memory)
-        tgt = tgt + self.dropout2(tgt2)
-
-        # MLP
-        tgt2 = self.linear2(self.dropout(self.activation(self.linear1(self.norm2(tgt)))))
-        tgt = tgt + self.dropout3(tgt2)
-        return tgt, sa_weights, ca_weights
-
-    def forward(self, query:Tensor, context_h_c:Sequence[Tensor], context_f_c:Tensor, context_d_c:Sequence[Tensor], 
-                memory:Tensor, query_mask: Optional[Tensor] = None, context_key_padding_mask: Optional[Tensor] = None):
-        query_norm = self.norm_q(query)
-        context_h_c_norm = (self.norm_context_h_c[i](context_h_c_i) for i, context_h_c_i in enumerate(context_h_c))
-        context_f_c_norm = self.norm_contxt_f_c(context_f_c)
-        context_d_c_norm = (self.norm_contxt_d_c[i](context_d_c_i) for i, context_d_c_i in enumerate(context_d_c))
-        query = self.forward_stream(query, query_norm, context_h_c_norm, context_f_c_norm, context_d_c_norm,
-                                    memory, query_mask, context_key_padding_mask)[0]
-        return query
-
-class Decoder(pl.LightningModule):
-    __constants__ = ['norm']
-
-    def __init__(self, d_model:int , nhead:int, dim_feedforward= 2048, dropout= 0.1, activation='GELU', 
-                 layer_norm_eps=1e-5, num_h_c:int = 2, num_d_c:int = 2, num_layers:int = 1):
-        super().__init__()
-        self.layers = [DecoderLayer(d_model, nhead, dim_feedforward=dim_feedforward,
-                                    dropout= dropout, activation= activation,
-                                    layer_norm_eps=layer_norm_eps, 
-                                    num_h_c = num_h_c, num_d_c= num_d_c) for _ in range(num_layers)]
-        self.num_layers = num_layers
-        self.norm = nn.LayerNorm(d_model)
-
-    def forward(self, query:Tensor, context_h_c:Sequence[Tensor], context_f_c:Tensor, context_d_c:Sequence[Tensor],
-                memory, query_mask: Optional[Tensor] = None, context_key_padding_mask: Optional[Tensor] = None):
-        for layer in self.layers:
-            query = layer(query, context_h_c, context_f_c, context_d_c, memory, query_mask, context_key_padding_mask)
-        query = self.norm(query)
-        return query
-
-class TokenEmbedding(pl.LightningModule):
-
-    def __init__(self, h_c_charset_size: int, f_c_charset_size:int, d_c_charset_size:int, 
-                 embed_dim: int, num_h_c:int = 2, num_d_c:int = 2):
-        super().__init__()
-        self.h_c_embedding = [nn.Embedding(num_embeddings=h_c_charset_size, embedding_dim=embed_dim)
-                                for _ in range(num_h_c)]
-        self.f_c_embedding = nn.Embedding(num_embeddings=f_c_charset_size, embedding_dim=embed_dim)
-        self.d_c_embedding = [nn.Embedding(num_embeddings=d_c_charset_size, embedding_dim=embed_dim)
-                                for _ in range(num_d_c)]
-        self.embed_dim = embed_dim
-
-    def forward(self, h_c_tokens: Sequence[Tensor], f_c_tokens:Tensor, d_c_tokens:Sequence[Tensor]):
-        h_c_emb = [math.sqrt(self.embed_dim) * self.h_c_embedding[i](h_c_t) for i,h_c_t in enumerate(h_c_tokens)]
-        f_c_emb = math.sqrt(self.embed_dim) * self.f_c_embedding(f_c_tokens)
-        d_c_emb = [math.sqrt(self.embed_dim) * self.d_c_embedding[i](d_c_t) for i, d_c_t in enumerate(d_c_tokens)]
-        return h_c_emb, f_c_emb, d_c_emb
 
 class HindiPARSeq(HindiBaseSystem):
 
@@ -242,8 +102,8 @@ class HindiPARSeq(HindiBaseSystem):
         enc_param_names = {'encoder.' + n for n in self.encoder.no_weight_decay()}
         return param_names.union(enc_param_names)
 
-    def decode(self, query:Tensor, h_c_ctx:Sequence[Tensor], f_c_ctx:Tensor, d_c_ctx:Sequence[Tensor], memory:Tensor,
-                ctx_padding_mask: Optional[Tensor] = None, query_mask: Optional[Tensor] = None):
+    def decode(self, h_c_ctx:Sequence[Tensor], f_c_ctx:Tensor, d_c_ctx:Sequence[Tensor], memory:Tensor,
+                ctx_padding_mask: Optional[Tensor] = None, query:Optional[Tensor] = None, query_mask: Optional[Tensor] = None):
         N, L = f_c_ctx.shape
         # <bos> stands for the null context. We only supply position information for characters after <bos>.
         h_c_null_ctx, f_c_null_ctx, d_c_null_ctx = self.text_embed(
@@ -433,32 +293,57 @@ class HindiPARSeq(HindiBaseSystem):
         return content_mask, query_mask
 
     def training_step(self, batch, batch_idx):
-        images, labels = batch
-        tgt = self.tokenizer.encode(labels, self.device)
+        imgs, labels = batch
+        batch_size = len(labels)
+        h_c_2_targets, h_c_1_targets, f_c_targets, d_c_targets = (
+                                  torch.zeros(batch_size, self.max_grps, device= self.device, dtype= torch.long),
+                                  torch.zeros(batch_size, self.max_grps, device= self.device, dtype= torch.long),
+                                  torch.zeros(batch_size, self.max_grps, device= self.device, dtype= torch.long),
+                                  torch.zeros(batch_size, self.max_grps, self.num_d_classes, device= self.device),
+                                )
+        
+        n_grps = [self.max_grps for i in range(batch_size)]
+        for idx,label in enumerate(labels, start= 0):
+            h_c_2_targets[idx], h_c_1_targets[idx], f_c_targets[idx], d_c_targets[idx], n_grps[idx] = self.tokenizer.label_encoder(label, device= self.device)
 
         # Encode the source sequence (i.e. the image codes)
-        memory = self.encode(images)
+        memory = self.encode(imgs)
 
         # Prepare the target sequences (input and output)
-        tgt_perms = self.gen_tgt_perms(tgt)
-        tgt_in = tgt[:, :-1]
-        tgt_out = tgt[:, 1:]
+        tgt_perms = self.gen_tgt_perms(f_c_targets)
+        # ignore BOS tag
+        h_c_2_out, h_c_1_out, f_c_out, d_c_out = h_c_2_targets[:, 1:], h_c_1_targets[:,1:], f_c_targets[:,1:], d_c_targets[:,1:]
         # The [EOS] token is not depended upon by any other token in any permutation ordering
-        tgt_padding_mask = (tgt_in == self.pad_id) | (tgt_in == self.eos_id)
+        ctx_padding_mask = (f_c_out == self.pad_id) | (f_c_out == self.eos_id) # pads and eos are same accross char grps
 
         loss = 0
         loss_numel = 0
-        n = (tgt_out != self.pad_id).sum().item()
+        n = (f_c_out != self.pad_id).sum().item()
         for i, perm in enumerate(tgt_perms):
             tgt_mask, query_mask = self.generate_attn_masks(perm)
-            out = self.decode(tgt_in, memory, tgt_mask, tgt_padding_mask, tgt_query_mask=query_mask)
-            logits = self.head(out).flatten(end_dim=1)
-            loss += n * F.cross_entropy(logits, tgt_out.flatten(), ignore_index=self.pad_id)
+            vals, indices = torch.topk(d_c_targets, k= 2)
+            out = self.decode(h_c_ctx= [h_c_2_targets[:,:-1], h_c_1_targets[:,:-1]], f_c_ctx= f_c_targets[:,:-1], 
+                              d_c_ctx= [indices[:,:-1,i] for i in range(2)], # ignore the additional target for EOS
+                              memory= memory, query_mask=query_mask, ctx_padding_mask= ctx_padding_mask)
+            
+            (h_c_2_logits, h_c_1_logits, f_c_logits, d_logits) = self.classifier(out)
+            # loss += n * F.cross_entropy(logits, tgt_out.flatten(), ignore_index=self.pad_id)
+            # Get the flattened versions of the targets and the logits for grp level metrics
+            ((flat_h_c_2_targets, flat_h_c_1_targets, flat_f_c_targets, flat_d_c_targets), 
+            (flat_h_c_2_logits, flat_h_c_1_logits, flat_f_c_logits, flat_d_c_logits)) = self._get_flattened_non_pad(
+                                                                                    targets= (h_c_2_out, h_c_1_out, f_c_out, d_c_out),
+                                                                                    logits= (h_c_2_logits, h_c_1_logits, f_c_logits, d_logits),
+                                                                                )
+            loss += self.h_c_2_loss(input= flat_h_c_2_logits, target= flat_h_c_2_targets) \
+            + self.h_c_1_loss(input= flat_h_c_1_logits, target= flat_h_c_1_targets) \
+            + self.f_c_loss(input= flat_f_c_logits, target= flat_f_c_targets) \
+            + self.d_loss(input= flat_d_c_logits, target= flat_d_c_targets)
+            loss *= n
             loss_numel += n
             # After the second iteration (i.e. done with canonical and reverse orderings),
             # remove the [EOS] tokens for the succeeding perms
             if i == 1:
-                tgt_out = torch.where(tgt_out == self.eos_id, self.pad_id, tgt_out)
+                tgt_out = torch.where(f_c_out == self.eos_id, self.pad_id, f_c_out)
                 n = (tgt_out != self.pad_id).sum().item()
         loss /= loss_numel
 
