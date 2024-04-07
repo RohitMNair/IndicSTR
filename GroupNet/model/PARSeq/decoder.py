4c68d07ecf723a6fbe020203fa600d7b0b6f8c87
Rohit Nair <rohitn@dnat.simula.no>
2024-04-07 16:58:36 +0200

    renamed Decoder variables

--- GroupNet/model/PARSeq/decoder.py
+++ GroupNet/model/PARSeq/decoder.py
@@ -1,5 +1,5 @@
 from torch import Tensor
-from typing import Optional, Sequence
+from typing import Optional, Sequence, Union
 from model.head import get_activation
 import lightning.pytorch as pl
 import torch.nn as nn
@@ -11,31 +11,50 @@ class DecoderLayer(pl.LightningModule):
     A Transformer decoder layer supporting two-stream attention (XLNet)
     This implements a pre-LN decoder, as opposed to the post-LN default in PyTorch.
     """
-
-    def __init__(self, d_model, nhead, dim_feedforward=2048, dropout=0.1, activation='GELU',
-                 layer_norm_eps=1e-5, num_h_c:int = 2, num_d_c:int = 2):
+    def __init__(self, d_model:int, nhead_self_attn:Sequence[Union[int,Sequence[int]]], nhead_cross_attn:int,
+                 dim_feedforward:int =2048, dropout:float =0.1, activation:str ='GELU', layer_norm_eps:float =1e-5, 
+                 num_h_c:int = 2, num_d_c:int = 2):
         """
         constructor for DecoderLayer
         Args:
-        - d_model (int): dimension of the decoder feature vectors
+        - d_model (int): dimension of the decoder feature vectors (must be divisible by 3)
+        - nhead_self_attn (Sequence[Union[int,Sequence[int]]]): number of multi-head attention heads for each character category
+                            -- index-0: Sequence[int] for nheads of half-character
+                            -- index-1: int for nheads for full-character
+                            -- index-2: Sequnce[int] for nheads of diacritic-character
+        - nhead_cross_attn (int): number of attention heads for cross attention
+        - dim_feedforwad (int, default= 2048): dimension of the intermediate Dense or Linear layer in the MLP
+        - dropout (float, default= 0.1): dropout probability
+        - activation (str, default= 'GELU'): string name of activation as specified in torch.nn
+        - layer_norm_eps (float, default= 1e-5): value of epsilon in layer norm
+        - num_h_c (int, default= 2): number of halc-character classes
+        - num_d_c (int, default= 2): number of diacritic classes
 
+        # Note: the specified heads must evenly divide d_model/3 and for each Sequence element is the
+        nhead, the Sequence elements must evenly divide d_model/(3 * len(seq))
+        eg: [[2,2], 4, [2,2]] here 2 heads for each half_character, 4 heads for full character 
+            and 2 heads for each diacritic character. if the d_model is 768 ( 768 % 3 == 0)
+            and for half-char-1 it will be 2 heads with 64 dim each ((768/(3 * 2)) % 2 == 0) similarly for half char 2. 
+            Full character will have 4 heads with 64 dim. each. Diacritic will be same as half-char
         """
         super().__init__()
+        self.d_model = d_model
+        self.nhead_self_attn = nhead_self_attn
+        self.nhead_cross_attn = nhead_cross_attn
+        self.dim_feedforward = dim_feedforward
+        self.activation = activation
+        self.dropout = dropout
+        self.layer_norm_eps = layer_norm_eps
         self.num_h_c = num_h_c
         self.num_d_c = num_d_c
-        self.self_attn_h_c = [nn.MultiheadAttention(d_model//(num_h_c * 3), nhead / self.num_h_c, 
+        # 0th index will correspond to h_c_n and ith index will correspond to h_c_(n-i)
+        self.self_attn_h_c = [nn.MultiheadAttention(d_model // (3 * len(nhead_self_attn[0])), nhead_self_attn[0], 
                                                     dropout= dropout, batch_first= True) for _ in range(num_h_c)]
-        for _ in range(self.num_h_c):
-            # 0th index will correspond to h_c_n and ith index will correspond to h_c_(n-i)
-            self.self_attn_h_c.append(
-                                        nn.MultiheadAttention(d_model//(num_h_c * 3), nhead / self.num_h_c, 
-                                                                dropout= dropout, batch_first= True)
-                                    )
-        
-        self.self_attn_f_c = nn.MultiheadAttention(d_model // 3, nhead, dropout=dropout, batch_first=True)
-        self.self_attn_d_c = [nn.MultiheadAttention(d_model // (num_d_c * 3), nhead / num_d_c,
+        self.self_attn_f_c = nn.MultiheadAttention(d_model // 3, nhead_self_attn[1], dropout=dropout, batch_first=True)
+        self.self_attn_d_c = [nn.MultiheadAttention(d_model // (3 * len(nhead_self_attn[2])), nhead_self_attn[2],
                                                     dropout=dropout, batch_first=True) for _ in range(num_d_c)]
-        self.cross_attn = nn.MultiheadAttention(d_model, nhead * 3, dropout=dropout, batch_first=True)
+        
+        self.cross_attn = nn.MultiheadAttention(d_model, nhead_cross_attn, dropout=dropout, batch_first=True)
 
         # for merging representations
         self.merge1 = nn.Linear(d_model, dim_feedforward)
@@ -63,8 +82,8 @@ class DecoderLayer(pl.LightningModule):
 
         self.activation = get_activation(activation)()
 
-    def forward_stream(self, tgt: Tensor, tgt_norm: Tensor, tgt_h_c_kv: tuple, tgt_f_c_kv:Tensor, tgt_d_c_kv:Tensor,
-                       memory: Tensor, tgt_mask: Optional[Tensor], tgt_key_padding_mask: Optional[Tensor]):
+    def forward_stream(self, query:Tensor, query_norm:Tensor, h_c_kv:Sequence[Tensor], f_c_kv:Tensor, d_c_kv:Sequence[Tensor],
+                       memory:Tensor, query_mask:Optional[Tensor], key_padding_mask:Optional[Tensor]):
         """
         Forward pass for a single stream (i.e. content or query)
         tgt_norm is just a LayerNorm'd tgt. Added as a separate parameter for efficiency.
@@ -72,32 +91,32 @@ class DecoderLayer(pl.LightningModule):
         memory is LayerNorm'd by ViT.
         """
         # self-attn
-        tgt2_h_c, sa_weights_h_c = [], []
-        for self_attn, tgt_h_c in zip(self.self_attn_h_c, tgt_h_c_kv):
-            tgt_, sa_ = self_attn(tgt_norm, tgt_h_c, attn_mask=tgt_mask, key_padding_mask=tgt_key_padding_mask)
-            tgt2.append(tgt_)
-            sa_weights_h_c.append(sa_)
-        tgt2_f_c, sa_weights_f_c = self.self_attn_f_c(tgt_norm, tgt_f_c_kv, attn_mask=tgt_mask, key_padding_mask=tgt_key_padding_mask)
-        tgt2_d_c, sa_weights_d_c = []
-        for self_attn, tgt_d_c in zip(self.self_attn_d_c, tgt_d_c_kv):
-            tgt_, sa_ = self.self_attn_d_c(tgt_norm, tgt_d_c, attn_mask=tgt_mask, key_padding_mask=tgt_key_padding_mask)
-            tgt2_d_c.append(tgt_)
-            sa_weights_d_c.append(sa_)
+        sa_h_c, sa_weights_h_c = [], []
+        for self_attn, h_c in zip(self.self_attn_h_c, h_c_kv):
+            sa_, w_= self_attn(query_norm, h_c, attn_mask=query_mask, key_padding_mask=key_padding_mask)
+            sa_h_c.append(sa_)
+            sa_weights_h_c.append(w_)
+        sa_f_c, sa_weights_f_c = self.self_attn_f_c(query_norm, f_c_kv, attn_mask=query_mask, key_padding_mask=key_padding_mask)
+        sa_d_c, sa_weights_d_c = []
+        for self_attn, d_c in zip(self.self_attn_d_c, d_c_kv):
+            sa_, w_ = self.self_attn_d_c(query_norm, d_c, attn_mask=query_mask, key_padding_mask=key_padding_mask)
+            sa_d_c.append(sa_)
+            sa_weights_d_c.append(w_)
         
         # Merge
-        tgt2 = torch.cat([*tgt2_h_c, tgt2_f_c, *tgt2_d_c], dim= -1)
+        sa = torch.cat([*sa_h_c, sa_f_c, *sa_d_c], dim= -1)
         sa_weights = (tuple(sa_weights_h_c), sa_weights_f_c, sa_weights_d_c)
-        tgt2 = self.merge2(self.dropout_merge1(self.activation_merge(self.merge1(self.norm_merge(tgt2)))))
-        tgt = tgt + self.dropout_merge2(tgt2)
+        sa = self.merge2(self.dropout_merge1(self.activation_merge(self.merge1(self.norm_merge(sa)))))
+        query = query + self.dropout_merge2(sa)
         
         # cross attn
-        tgt2, ca_weights = self.cross_attn(self.norm1(tgt), memory, memory)
-        tgt = tgt + self.dropout2(tgt2)
+        ca, ca_weights = self.cross_attn(self.norm1(query), memory, memory)
+        query = query + self.dropout2(ca)
 
         # MLP
-        tgt2 = self.linear2(self.dropout(self.activation(self.linear1(self.norm2(tgt)))))
-        tgt = tgt + self.dropout3(tgt2)
-        return tgt, sa_weights, ca_weights
+        out = self.linear2(self.dropout(self.activation(self.linear1(self.norm2(query)))))
+        query = query + self.dropout3(out)
+        return query, sa_weights, ca_weights
 
     def forward(self, query:Tensor, context_h_c:Sequence[Tensor], context_f_c:Tensor, context_d_c:Sequence[Tensor], 
                 memory:Tensor, query_mask: Optional[Tensor] = None, context_key_padding_mask: Optional[Tensor] = None):
@@ -112,10 +131,13 @@ class DecoderLayer(pl.LightningModule):
 class Decoder(pl.LightningModule):
     __constants__ = ['norm']
 
-    def __init__(self, d_model:int , nhead:int, dim_feedforward= 2048, dropout= 0.1, activation='GELU', 
+    def __init__(self, d_model:int,nhead_self_attn:Sequence[Union[int,Sequence[int]]], nhead_cross_attn:int, 
+                 dim_feedforward= 2048, dropout= 0.1, activation='GELU', 
                  layer_norm_eps=1e-5, num_h_c:int = 2, num_d_c:int = 2, num_layers:int = 1):
         super().__init__()
-        self.layers = [DecoderLayer(d_model, nhead, dim_feedforward=dim_feedforward,
+        self.layers = [DecoderLayer(d_model, nhead_self_attn= nhead_self_attn,
+                                    nhead_cross_attn= nhead_cross_attn,
+                                    dim_feedforward=dim_feedforward,
                                     dropout= dropout, activation= activation,
                                     layer_norm_eps=layer_norm_eps, 
                                     num_h_c = num_h_c, num_d_c= num_d_c) for _ in range(num_layers)]
@@ -132,7 +154,7 @@ class Decoder(pl.LightningModule):
 class TokenEmbedding(pl.LightningModule):
 
     def __init__(self, h_c_charset_size: int, f_c_charset_size:int, d_c_charset_size:int, 
-                 embed_dim: int, num_h_c:int = 2, num_d_c:int = 2):
+                 embed_dim:int, num_h_c:int = 2, num_d_c:int = 2):
         super().__init__()
         self.h_c_embedding = [nn.Embedding(num_embeddings=h_c_charset_size, embedding_dim=embed_dim)
                                 for _ in range(num_h_c)]
